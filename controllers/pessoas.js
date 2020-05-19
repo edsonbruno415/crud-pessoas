69c93970b0495fe20f2ef662366dd2bb50d018fc
Edson Bruno <edsonbruno415@gmail.com>
2020-05-19 20:57:34 -0300

    Add pagination to people list

--- controllers/pessoas.js
+++ controllers/pessoas.js
@@ -1,8 +1,29 @@
 const init = db => {
 
     async function index(req, res) {
-        const pessoas = await db("pessoas").select("*");
-        return res.render("pessoas", { pessoas });
+        let { sizePage, page } = req.query;
+        if(!sizePage || sizePage<= 0){
+            sizePage = 10;
+            page = 0;
+        }
+        const totalPessoas = await db("pessoas").count("*",{as: "total"});
+        let totalPages = parseInt(totalPessoas[0].total/ sizePage) + 1;
+        if(page >= totalPages || page <= 0){
+            page = 0;
+        }
+        if(sizePage>= totalPessoas[0].total){
+            sizePage = 10;
+            totalPages = parseInt(totalPessoas[0].total/ sizePage) + 1;
+        }
+        const offset = parseInt(page*sizePage);
+        const limit = parseInt(sizePage);
+        const pessoas = await db("pessoas").select("*").offset(offset).limit(limit);
+        const pagination = {
+            page,
+            sizePage,
+            totalPages
+        }
+        return res.render("pessoas", { pessoas, pagination });
     }
 
     async function deleteOne(req, res) {
@@ -26,7 +47,7 @@ const init = db => {
         const pessoa = await db("pessoas").limit(1).where({ id }).select("*");
         return res.render("update", { pessoa: pessoa[0] });
     }
-    
+
     async function update(req, res) {
         const { id, nome, nascimento, cargo } = req.body;
         await db("pessoas").where({ id }).update({ nome, nascimento, cargo });
